a5d014b3ad97fbc7625d161fe06e54d5a13085c9
Vlad Silverman <vsilverman@gmail.com>
2018-11-12 21:00:23 -0800

    Update MyArt.java

--- MyArt.java
+++ MyArt.java
@@ -1,8 +1,5 @@
 import java.io.File;
 
-/**
- * 
- */
 
 /**
  * -----------------------------------------------------------------------------
@@ -10,8 +7,7 @@ import java.io.File;
  * 
  * @version 1.0
  * @author  Vlad
- * @author  (vsilverman@yahoo.com)
- * @author  http://www.yahoo.com
+ * @author  (vsilverman@gmail.com)
  * -----------------------------------------------------------------------------
  *
  */
@@ -23,18 +19,20 @@ public class MyArt {
 	private static String dirName = "d:\\Flash1\\SVA\\R\\V\\2016";
 	private static String cmdName = "call-from-java.cmd";
 	
-	private static void publish(String artProps) {
+	public static void publish(String artProps) {
 		MyProperties.propsFileName = artProps;
 		myProps.loadAndPrint();
 		dirName = MyProperties.props.getProperty("art.folder");
 		cmdName = MyProperties.props.getProperty("art.script");
-		
+
 		traverse(new File(dirName));
 	}
 
     private static void processDir(File dir) {
-        System.out.print( (dir.isDirectory() ? "[D] : " : "[F] : "));
-        System.out.println(dir);
+	    if (dir != null) {
+            System.out.print( (dir.isDirectory() ? "[D] : " : "[F] : "));
+            System.out.println(dir);
+        }
     }
 
     private static void traverse(File dir) {
@@ -42,28 +40,34 @@ public class MyArt {
 
         if (dir.isDirectory()) {
             String[] children = dir.list();
-            for (int i=0; i<children.length; i++) {
-                traverse(new File(dir, children[i]));
-            }
+            for (String child : children)
+                traverse(new File(dir, child));
         }
         else {
         	String fileName = dir.toString();
-        	int pos = fileName.lastIndexOf("\\");
+        	String pathSep = dependOnPlatform("\\", "/");
+        	int pos = fileName.lastIndexOf(pathSep);
         	String scriptName = fileName.substring(pos + 1);
         	if (scriptName.equals(cmdName)) {
             	mc.exec(fileName);
         	}
         }
+    }
 
+    private static String dependOnPlatform(String WinValue, String LinuxValue) {
+	    String res = WinValue;
+        String os = System.getProperty("os.name");
+        if (os.contains("Linux") || os.contains("Mac"))
+            res = LinuxValue;
+        return res;
     }
 
 	/**
-	 * @param args
+	 * @param args property file
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//    	String PropFile = "d:\\Flash1\\SVA\\R\\V\\2016\\art.cfg"; 
-    	String PropFile = "c:\\bin\\art.cfg"; 
+    	String PropFile = dependOnPlatform(".\\cfg\\art.cfg", "./art.cfg");
+
 		if (args.length < 1) {
 			System.out.println("Usage: java MyArt PropFile");
 			System.out.println("Running demo version with default params");
@@ -76,3 +80,4 @@ public class MyArt {
 	}
 
 }
+
